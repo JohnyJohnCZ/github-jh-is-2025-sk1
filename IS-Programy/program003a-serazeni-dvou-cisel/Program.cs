a92e5ea919951c09cbcbbc888bb190a0e51faaf1
Jan Honzíček <honz.honzicek@gmail.com>
2025-10-22 10:01:36 +0200

    #5 Největší ze tří čísel - Program

---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,42 @@
+﻿string again = "a"; // jedno rovná se - přiřazení hodnoty, dvě rovná se == porovnání hodnot
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*************************************");
+    Console.WriteLine("******** Seřazení dvou čísel ********");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("**********  Jan Honzíček  ***********");
+    Console.WriteLine("*************************************");
+    Console.WriteLine();
+
+    //vstup hodnoty do programu
+
+    Console.WriteLine("Zadejte celé číslo - hodnota A");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím hodnotu A znovu:");
+    }
+    Console.WriteLine("Zadejte celé číslo - hodnota B");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím hodnotu B znovu:");
+    }
+
+    Console.WriteLine();
+    int pom;
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných");
+    }
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+    Console.WriteLine("");
+    Console.WriteLine("Pro opakování programu stiskněte 'a', pro ukončení jinou klávesu.");
+    again = Console.ReadLine();
+
+
+}
---
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,62 @@
+﻿string again = "a"; // jedno rovná se - přiřazení hodnoty, dvě rovná se == porovnání hodnot
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*************************************");
+    Console.WriteLine("******** Seřazení dvou čísel ********");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("**********  Jan Honzíček  ***********");
+    Console.WriteLine("*************************************");
+    Console.WriteLine();
+
+    //vstup hodnoty do programu
+
+    Console.WriteLine("Zadejte celé číslo - hodnota A");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím hodnotu A znovu:");
+    }
+    Console.WriteLine("Zadejte celé číslo - hodnota B");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím hodnotu B znovu:");
+    }
+    Console.WriteLine("Zadejte celé číslo - hodnota C");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím hodnotu C znovu:");
+    }
+
+    Console.WriteLine("=====================================");
+
+    if (a > b)
+    {
+        if (a > c)
+        Console.WriteLine($"Největší číslo je A = {a}");
+        else
+        Console.WriteLine($"Největší číslo je C = {c}");
+    }
+    else
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"Největší číslo je B = {b}");
+        }
+        else
+        {
+            Console.WriteLine($"Největší číslo je C = {c}");
+        }
+    }
+    Console.WriteLine("=====================================");
+
+
+
+    Console.WriteLine("");
+    Console.WriteLine("Pro opakování programu stiskněte 'a', pro ukončení jinou klávesu.");
+    again = Console.ReadLine();
+
+
+}
