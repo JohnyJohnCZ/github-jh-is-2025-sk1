8be73cd09a81e4dc489652c9f4a0bdb70b110397
Jan Honzíček <honz.honzicek@gmail.com>
2025-10-29 19:02:45 +0100

    #7 Pravoúhlý trojúhelník - Hotový kód

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,48 @@
+﻿string again = "a"; // jedno rovná se - přiřazení hodnoty, dvě rovná se == porovnání hodnot
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**************************************");
+    Console.WriteLine("******** Vykreslení obdélníku ********");
+    Console.WriteLine("**************************************");
+    Console.WriteLine("**********  Jan Honzíček  ************");
+    Console.WriteLine("**************************************");
+    Console.WriteLine();
+
+    //vstup hodnoty do programu
+
+    Console.WriteLine("Zadejte celé šířku obrazce (celé číslo):");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím šířku obrazce znovu:");
+    }
+    Console.WriteLine("Zadejte výšku obrazce (celé číslo):");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím výšku obrazce znovu:");
+    }
+    
+
+    Console.WriteLine("=====================================");
+
+   for (int i = 1; i <= height; i++)
+   {
+       for (int j = 1; j <= width; j++)
+       {
+            Console.Write(" * ");
+
+       }
+       Console.WriteLine();
+   }
+    Console.WriteLine("=====================================");
+
+
+
+    Console.WriteLine("");
+    Console.WriteLine("Pro opakování programu stiskněte 'a', pro ukončení jinou klávesu.");
+    again = Console.ReadLine();
+
+
+}
---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,36 @@
+﻿string again = "a"; // jedno rovná se - přiřazení hodnoty, dvě rovná se == porovnání hodnot
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("*** Vykreslení pravoúhlého trojúhelníku ***");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("************  Jan  Honzíček  **************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu
+    Console.WriteLine("Zadejte délku odvěsny trojúhelníku (celé číslo):");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte prosím délku odvěsny znovu:");
+    }
+
+    Console.WriteLine("===========================================");
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write(" * ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("===========================================");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte 'a', pro ukončení jinou klávesu.");
+    again = Console.ReadLine();
+}
